6796550669b4b4d53200c8fecd5fb20e1aaefd23
Artem Kulikov <chigar2010@yandex.ru>
2023-11-23 04:48:48 +0200

    upd

--- birthday_present/bot/models.py
+++ birthday_present/bot/models.py
@@ -17,12 +17,19 @@ def validate_not_future_date(value):
 
 
 class Friend(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=255)
     birthday = models.DateField(validators=[validate_not_future_date])
-    presents = models.ArrayField(models.CharField(max_length=100), null=True)
     user = models.ForeignKey(
         TelegramUser, related_name='friends', on_delete=models.CASCADE
     )
 
     def __str__(self):
         return self.name
+
+
+class Gift(models.Model):
+    friend = models.ManyToManyField(Friend, related_name='gifts', null=True)
+    present = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.present
