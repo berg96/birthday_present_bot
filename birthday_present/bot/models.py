971aea491e501d968de34f8419c5809e9566aacd
Artem Kulikov <chigar2010@yandex.ru>
2023-11-23 04:17:39 +0200

    init

--- birthday_present/bot/models.py
+++ birthday_present/bot/models.py
@@ -1,3 +1,27 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+def validate_not_future_date(value):
+    if value > timezone.now().date():
+        raise ValidationError("Дата не может быть в будущем.")
+
+
+class Friend(models.Model):
+    name = models.CharField(max_length=64)
+    birthday = models.DateField(validators=[validate_not_future_date])
+    presents = models.ArrayField(models.CharField(max_length=100), null=True)
+    user = models.ForeignKey(
+        User, related_name='friends', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
